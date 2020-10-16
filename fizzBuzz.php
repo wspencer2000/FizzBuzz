db2689a236efb5dbba7e994258d8de2bee76f6c1
Will Spencer <wspencer2000@gmail.com>
2020-10-16 16:25:59 -0500

    Initial commit

---
+++ fizzBuzz.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <?php
+
+            $number = 1;
+
+            while ($number <= 100){
+                
+                #Checks for numbers divisible by 3 AND 5
+                
+                if($number % 3 == 0 && $number % 5 == 0){
+                    print "<h1>FizzBuzz</h1>";
+                }
+                
+                #Checks for numbers divisible by 3
+
+                else if($number % 3 == 0){
+                    print "<p>Fizz</p>";
+                }
+                
+                #checks for numbers divisible by 5
+
+                else if($number % 5 == 0){
+                    print "<p>Buzz</p>";
+                }
+                
+                #prints number if no divisor detected
+
+                else{
+                    print '<p>'. $number . '</p>';
+                }
+
+                #Increments number
+                
+                $number++;
+            }
+        ?>
+    </body>
+</html>
